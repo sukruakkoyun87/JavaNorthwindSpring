07ef7e0273ddbcaac54fffaa6a06b80959614909
Win10 <sukruakkoyun87@gmail.com>
2021-05-09 00:03:24 +0300

    Ara

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,23 @@
+package kodlamaio.northwind.business.concretes;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ProductManager implements ProductService {
+
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+}
---
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -0,0 +1,93 @@
+package kodlamaio.northwind.entities.concretes;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private int id;
+
+    @Column(name = "category_id")
+    private int categoryid;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    @Column(name = "units_in_stock")
+    private short unitsInStock;
+
+    @Column(name = "quantity_per_unit")
+    private String quantityPerUnit;
+
+    public Product(int id, int categoryid, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
+        this.id = id;
+        this.categoryid = categoryid;
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.unitsInStock = unitsInStock;
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public Product() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(int categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public short getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(short unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+}
